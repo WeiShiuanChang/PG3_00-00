c4c1af83557046c85d8bdb677638019e672edd32
WeiShiuanChang <k024g1014@g.neec.ac.jp>
2025-06-27 15:00:31 +0900

    STLとコンテナクラス

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include<list>
 
 int main() {
+	// 1970年の山手線の駅名リスト
 	std::list <const char*> eki_list{
 		"Tokyo", "Kanda", "Akihabara", 
 		"Okachimachi", "Ueno", "Uguisudani",
@@ -13,30 +14,31 @@ int main() {
 		"Gotanda", "Osaki", "Sinagawa", 
 		"Tamachi", "Hamamatsucho", "Shimbashi","Yurakucho"
 	};
+	// 1970年の山手線の駅を出力
 	std::cout << "-------------------------------\n1970 Yamanote Line\n";
 	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr) {
 		std::cout << *itr << "\n";
 	}
-	//
+	//西日暮里駅は1971年に追加されたので、「田端」の前に挿入する
 	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr) {
 		if (*itr == "Tabata") {
 			itr = eki_list.insert(itr, "Nishi-Nippori");
 			++itr;
 		}
 	}
-	
+	// 2019年時点の山手線の駅を出力
 	std::cout << "-------------------------------\n2019 Yamanote Line\n";
 	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr) {
 		std::cout << *itr << "\n";
 	}
-
+	// 高輪ゲートウェイ駅は2020年に追加されたので、「田町」の前に挿入する
 	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr) {
 		if (*itr == "Tamachi") {
 			itr = eki_list.insert(itr, "Takanawa Gateway");
 			++itr;
 		}
 	}
-
+	// 2022年時点の山手線の駅を出力
 	std::cout << "-------------------------------\n2022 Yamanote Line\n";
 	for (auto itr = eki_list.begin(); itr != eki_list.end(); ++itr) {
 		std::cout << *itr << "\n";
